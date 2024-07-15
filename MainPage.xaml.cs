2b97210577a786aaf81907cdac6cd88c8d6fddeb
Jer542 <78572182+Jer542@users.noreply.github.com>
2024-07-15 14:08:58 -0400

    all business demo

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,56 +1,138 @@
-﻿namespace App
+﻿using App.Models;
+using System.Collections.Generic;
+
+namespace App
 {
     public partial class MainPage : ContentPage
     {
         private double totalMoney = 0;
-        private int lemonadeStandCount = 1;
-        private double moneyPerClick = 1;
+        private List<Business> businesses = new List<Business>();
 
         public MainPage()
         {
             InitializeComponent();
-            UpdateLemonadeStandCountDisplay();
-        }
-
-        private void OnBusinessButtonClicked(object sender, EventArgs e)
-        {
-            totalMoney += moneyPerClick * lemonadeStandCount;
+            InitializeBusinesses();
             UpdateTotalMoneyDisplay();
         }
 
-        private void OnUpgradeButtonClicked(object sender, EventArgs e)
+        private void InitializeBusinesses()
         {
-            const double upgradeCost = 10;
-            if (totalMoney >= upgradeCost)
+            businesses.Add(new Business(
+                "Lemonade Stands",
+                1,
+                10,
+                1,
+                LemonadeStandButton,
+                UpgradeLemonadeStandButton,
+                LemonadeStandCountLabel));
+
+            businesses.Add(new Business(
+                "Newspapers",
+                5,
+                50,
+                1,
+                NewspaperButton,
+                UpgradeNewspaperButton,
+                NewspaperCountLabel));
+
+            businesses.Add(new Business(
+                "Car Washes",
+                10,
+                100,
+                1,
+                CarWashButton,
+                UpgradeCarWashButton,
+                CarWashCountLabel));
+
+            businesses.Add(new Business(
+                "Pizza Shops",
+                20,
+                200,
+                1,
+                PizzaShopButton,
+                UpgradePizzaShopButton,
+                PizzaShopCountLabel));
+
+            businesses.Add(new Business(
+                "Donut Shops",
+                50,
+                500,
+                1,
+                DonutShopButton,
+                UpgradeDonutShopButton,
+                DonutShopCountLabel));
+
+            businesses.Add(new Business(
+                "Shrimp Boats",
+                100,
+                1000,
+                1,
+                ShrimpBoatButton,
+                UpgradeShrimpBoatButton,
+                ShrimpBoatCountLabel));
+
+            businesses.Add(new Business(
+                "Hockey Teams",
+                500,
+                5000,
+                1,
+                HockeyTeamButton,
+                UpgradeHockeyTeamButton,
+                HockeyTeamCountLabel));
+
+            businesses.Add(new Business(
+                "Movie Theaters",
+                1000,
+                10000,
+                1,
+                MovieTheaterButton,
+                UpgradeMovieTheaterButton,
+                MovieTheaterCountLabel));
+
+            businesses.Add(new Business(
+                "Banks",
+                5000,
+                50000,
+                1,
+                BankButton,
+                UpgradeBankButton,
+                BankCountLabel));
+
+            businesses.Add(new Business(
+                "Oil Refineries",
+                10000,
+                100000,
+                1,
+                OilRefineryButton,
+                UpgradeOilRefineryButton,
+                OilRefineryCountLabel));
+
+            foreach (var business in businesses)
             {
-                totalMoney -= upgradeCost;
-                lemonadeStandCount++;
-                UpdateTotalMoneyDisplay();
-                UpdateLemonadeStandCountDisplay();
+                business.GenerateButton.Clicked += (sender, e) => OnGenerateClicked(business);
+                business.UpgradeButton.Clicked += (sender, e) => OnUpgradeClicked(business);
+                business.UpdateCountLabel();
             }
         }
 
-        private void OnBuyMaxButtonClicked(object sender, EventArgs e)
+        private void OnGenerateClicked(Business business)
         {
-            const double upgradeCost = 10;
-            int maxAffordable = (int)(totalMoney / upgradeCost);
-            if (maxAffordable > 0)
+            business.GenerateIncome(ref totalMoney);
+            UpdateTotalMoneyDisplay();
+        }
+
+        private void OnUpgradeClicked(Business business)
+        {
+            if (business.Upgrade(ref totalMoney))
             {
-                lemonadeStandCount += maxAffordable;
-                totalMoney -= maxAffordable * upgradeCost;
+                business.UpdateCountLabel();
                 UpdateTotalMoneyDisplay();
-                UpdateLemonadeStandCountDisplay();
             }
         }
 
         private void UpdateTotalMoneyDisplay()
         {
             TotalMoneyLabel.Text = $"Total Money: ${totalMoney}";
-        }
-
-        private void UpdateLemonadeStandCountDisplay()
-        {
-            LemonadeStandCountLabel.Text = $"Lemonade Stands: {lemonadeStandCount}";
         }
     }
 }
---
+++ Models/Business.cs
@@ -0,0 +1,45 @@
+﻿namespace App.Models
+{
+    public class Business
+    {
+        public string Name { get; set; }
+        public double IncomePerClick { get; set; }
+        public double UpgradeCost { get; set; }
+        public int Count { get; set; }
+        public Button GenerateButton { get; set; }
+        public Button UpgradeButton { get; set; }
+        public Label CountLabel { get; set; }
+
+        public Business(string name, double incomePerClick, double upgradeCost, int count, Button generateButton, Button upgradeButton, Label countLabel)
+        {
+            Name = name;
+            IncomePerClick = incomePerClick;
+            UpgradeCost = upgradeCost;
+            Count = count;
+            GenerateButton = generateButton;
+            UpgradeButton = upgradeButton;
+            CountLabel = countLabel;
+        }
+
+        public void GenerateIncome(ref double totalMoney)
+        {
+            totalMoney += IncomePerClick * Count;
+        }
+
+        public bool Upgrade(ref double totalMoney)
+        {
+            if (totalMoney >= UpgradeCost)
+            {
+                totalMoney -= UpgradeCost;
+                Count++;
+                return true;
+            }
+            return false;
+        }
+
+        public void UpdateCountLabel()
+        {
+            CountLabel.Text = $"{Name}: {Count}";
+        }
+    }
+}
